88770cf8ec2632943d2bee5266cfe6df1f89c0e3
regulus33 <zacharyrowden89@gmail.com>
2017-02-24 19:01:41 -0500

    Working react components.

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::ProjectsController < Api::V1::BaseController
+  def index
+    respond_with Project.all
+  end
+
+  def create
+    respond_with :api, :v1, Project.create(project_params)
+  end
+
+  def destroy
+    respond_with Project.destroy(params[:id])
+  end
+
+  def update
+    project = Project.find(params["id"])
+    project.update_attributes(project_params)
+    respond_with project, json: project
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:id, :title, :description, :url)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	root to: 'site#index'
     namespace :api do
       namespace :v1 do
         resources :projects, only: [:index, :create, :destroy, :update]
