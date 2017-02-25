2d510804d931c59ba306e002f8c4f09da1217add
regulus33 <zacharyrowden89@gmail.com>
2017-02-24 19:59:32 -0500

    Create very basic react/project functionality.

--- app/assets/javascripts/components/_all_projects.js.jsx
+++ app/assets/javascripts/components/_all_projects.js.jsx
@@ -1,20 +1,30 @@
 var AllProjects = React.createClass({
 	
 	getInitialState() {
-		return { projects: []}
-	}
+		return { projects: [] }
+	},
 
 	componentDidMount() {
-		console.log("mounted");
-		$.getJSON('/api/v1/projects.json', (response) => {this.setState({projects: response})})
+		$.getJSON('/api/v1/projects.json', (response) => {this.setState({ projects: response }) });
 	},
 
 	render(){
+		debugger
+	  var projects = this.state.projects.map((project) => {
 		return (
-			<div>
+			<div key={project.id}>
 			  <h3>{project.title}</h3>
 			  <p>{project.description}</p>
 			  <img src={project.url}/>
 			</div>
+		)
+	});
+
+		return(
+	        <div>
+	            {projects}
+	        </div>
+	    )
+
 	}
 });
